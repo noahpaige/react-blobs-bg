7fe70804754efe46e93350319867297c72465705
noahpaige <noahlandonpaige@gmail.com>
2025-11-21 11:28:46 -0800

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import dts from "rollup-plugin-dts";
+
+export default [
+  // Generate JS bundles
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        declaration: true,
+        declarationDir: "./dist/types",
+      }),
+    ],
+    external: ["react", "react-dom"],
+  },
+  // Bundle type definitions
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: ["react", "react-dom"],
+  },
+];
+
